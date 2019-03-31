26a1a3e1807c2922252865508a0aee7d0ed90bab
Caridad Rivera <ccrive10@gmail.com>
2019-03-31 13:57:27 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -3,12 +3,14 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-  num_1 = Kernel.rand(1..20)
+   num_1 = Kernel.rand(1..20)
    num_2 = Kernel.rand(1..20)
    num_3 = Kernel.rand(1..20)
 
    if num_1==num_2 && num_2==num_3
-     resp.write "You Win"
+     resp.write "You Win"  ##the write method is like a puts statement
+    ## Web servers are just big ruby apps that respond to the user in an HTTP
+    ### response rather than via puts statements
    else
      resp.write "You Lose"
    end
